a468f6e1cd76b1922b54bb80b5357ac12ce1177b
hao <hao@echo>
2014-12-31 00:37:50 +0800

    Merge origin/master
    Conflicts:
    	source/controller/admin/articleController.class.php
    	source/controller/admin/baseController.class.php
    	source/controller/admin/modelController.class.php
    	source/event/editorEvent.class.php
    	static/admin/js/editor.js
    	static/common/js/plugin/jquery.selection.js
    	template/admin/article/edit.tpl
    	template/admin/nav.tpl
    	template/hao2014/common/header.tpl
    	template/hao2014/index/index.tpl

--- config/config.php
+++ config/config.php
@@ -3,8 +3,8 @@
 return array (
 	'DB_HOST' => 'localhost',
 	'DB_USER' => 'root',
-	'DB_PSWD' => '',
-	'DB_NAME' => 'haosblog',
+	'DB_PSWD' => '2948911',
+	'DB_NAME' => 'blog2014',
 	'DB_PRE' => 'hao_',
 	'WEBROOT' => '/',
 
